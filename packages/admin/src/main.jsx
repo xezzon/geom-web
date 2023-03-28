e2ba7255e019aeec1df23add51949eee493a5f8c
xezzon <yzjdx4399@gmail.com>
2023-03-28 22:52:47 +0800

    引入 postcss

--- packages/admin/src/main.jsx
+++ packages/admin/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
-import '@/assert/antd.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/assert/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
